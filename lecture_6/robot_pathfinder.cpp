dc5c23104f2e641ed4209cbbdec9f5f160e58424
arknamal <arknamal@gmail.com>
2024-09-11 21:10:45 +0500

    solution of problem 6e - pathfinding for robots in a grid

---
+++ lecture_6/robot_pathfinder.cpp
@@ -0,0 +1,12 @@
+// This program is part of the implementation of Lecture 6 of the CCI Course 2024 by Sarfraz Raza
+// Problem 6e:
+// Robot Pathfinding:
+// Provided an 2D array with multiple robots and obstacles (robot_map.txt), determine whether each can
+// reach the designated destination or not.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;
+}

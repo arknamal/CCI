498d6cf9f6392fda304f24fb2dd1a63813164913
arknamal <arknamal@gmail.com>
2024-10-30 21:48:23 +0500

    solution of 10a - initial commit

---
+++ lecture_10/printingTree.cpp
@@ -0,0 +1,17 @@
+// This program is part of the implementation of Lecture 10 of the CCI Course 2024 by Sarfraz Raza
+// Problem 6a:
+// Print Tree:
+// Write a recursive function outputTree to display a binary tree object on the screen. The
+// function should output the tree row by row, with the top of the tree at the left of the
+// screen and bottom of the tree towards the right of the screen. Each row is output vertically.
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+
+}
